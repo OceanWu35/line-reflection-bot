4d6d57f86d0fbf668903f37556f57b333e8f9f73
Wu Ocean <wuocean@Wus-Laptop-2.local>
2025-05-03 23:42:57 +0800

    修正 client 定義順序

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const line = require('@line/bot-sdk');
 
+// 放在最上面
 const config = {
   channelAccessToken: '***REMOVED***',
   channelSecret: '***REMOVED***'
 };
 
+const client = new line.Client(config); // ⬅️ 一定要放在 handleEvent 上面！
+
 const app = express();
 app.use(express.json());
+
 app.post('/webhook', line.middleware(config), (req, res) => {
   const events = req.body.events;
   Promise.all(events.map(handleEvent)).then(result => res.json(result));
@@ -24,10 +28,10 @@ function handleEvent(event) {
   return Promise.resolve(null);
 }
 
-const client = new line.Client(config);
 app.listen(3000, () => {
   console.log('機器人正在監聽 port 3000!');
 });
+
 // 測試用註解：確認 Git 推送流程是否順利
 
 
